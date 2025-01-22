7084a684bd701e0a3374c7c39c73162d0b43050a
devklynch <devklynch@gmail.com>
2025-01-22 10:33:11 -0700

    updated time and added icons

--- src/Shows/Shows.js
+++ src/Shows/Shows.js
@@ -1,4 +1,13 @@
 import "./Shows.css";
+import {
+  GiPalmTree,
+  GiDesert,
+  GiMusicSpell,
+  GiDesertSkull,
+  GiWorld,
+  GiLindenLeaf,
+  GiFlowerEmblem,
+} from "react-icons/gi";
 
 function Shows({
   artist,
@@ -13,13 +22,47 @@ function Shows({
     removeShow(show_id);
     fetchShows();
   };
+
+  const formatTime = (timeString) => {
+    const date = new Date(timeString);
+    date.setHours(date.getHours() + 7);
+
+    return date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+  };
+
+  const getLocationIcon = (location) => {
+    switch (location) {
+      case "Gobi":
+        return <GiDesertSkull className="location-icon" />;
+      case "Outdoor Theatre":
+        return <GiWorld className="location-icon" />;
+      case "Sonora":
+        return <GiLindenLeaf className="location-icon" />;
+      case "Mojave":
+        return <GiFlowerEmblem className="location-icon" />;
+      case "Sahara":
+        return <GiDesert className="location-icon" />;
+      case "Yuma":
+        return <GiMusicSpell className="location-icon" />;
+      case "Coachella":
+        return <GiPalmTree className="location-icon" />;
+      default:
+        return <GiPalmTree className="location-icon" />;
+    }
+  };
   return (
     <div className="show-card">
       <h4>{artist}</h4>
       <h5>Stage: {location}</h5>
+      <p>Show Time: {`${formatTime(start_time)} - ${formatTime(end_time)}`}</p>
       <p>
         Show Time: {start_time} - {end_time}
       </p>
+      {getLocationIcon(location)}
       <button onClick={handleDelete}>Remove Show</button>
     </div>
   );
